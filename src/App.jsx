d481eddf8f4e57292fb212daf540a831ab05363d
Tlou Kgatla <tloukgatla199@gmail.com>
2026-04-04 21:49:02 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Navigation_bar from './components/Navigation_bar'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Navigation_bar/>
+    </>
+  )
+}
+
+export default App
