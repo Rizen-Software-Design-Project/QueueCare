b75b8ed1c244b95f2e03fa3f8f6aa11fa6a04987
Nthabiseng Lefophana <nthabiseng.lefophana1@gmail.com>
2026-04-13 10:15:17 +0200

    deploying to github-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Validate from './components/id_validation'
-import Otp from './components/otp';
-import Login from './components/login'
+import Validate from "./components/id_validation";
+import Otp from "./components/otp";
+import Login from "./components/login";
 import Welcome from "./components/Welcome.jsx";
 import Signin from "./components/Signin.jsx";
 import Dashboard from "./components/Dashboard.jsx";
 import ClinicSearch from "./components/Clinic_search";
 import "./App.css";
-<BrowserRouter basename="/QueueCare"></BrowserRouter>
 
 function App() {
   return (
     <BrowserRouter basename="/QueueCare">
       <Routes>
         <Route path="/" element={<Welcome />} />
-        <Route path="/signin" element={<login />} />
+        <Route path="/signin" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/clinic-search" element={<ClinicSearch />} />
-        <Route path="/id_validation" element={<Validate/>}/>
-        <Route path="/otp" element={<Otp/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/id_validation" element={<Validate />} />
+        <Route path="/otp" element={<Otp />} />
       </Routes>
     </BrowserRouter>
   );
