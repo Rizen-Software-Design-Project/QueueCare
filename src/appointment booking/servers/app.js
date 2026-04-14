1a5fe4c1d814f29c477610a0925aa34bd129811c
somnothopako <somnoto@pako.co.za>
2026-04-14 23:15:40 +0200

    I fixed isssues with the URL as it was calling the local host and not the deployment frontend in azure

--- src/appointment booking/servers/app.js
+++ src/appointment booking/servers/app.js
@@ -1,15 +1,31 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import appointmentRoutes from '../routes/appointmentRoutes.js';
 import notFound from '../middleware/notFound.js';
 import errorHandler from '../middleware/errorHandler.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const distPath = path.join(__dirname, '../../../dist');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use('/', appointmentRoutes);
+
+// Serve the built React frontend
+app.use(express.static(distPath));
+app.get('*', (req, res, next) => {
+  res.sendFile(path.join(distPath, 'index.html'), (err) => {
+    if (err) {
+      next();
+    }
+  });
+});
+
 app.use(notFound);
 app.use(errorHandler);
 
