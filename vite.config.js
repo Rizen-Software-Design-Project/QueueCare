97e63038296cb74076b42fc86e68c6baa83dca1f
Nthabiseng Lefophana <nthabiseng.lefophana1@gmail.com>
2026-04-14 21:55:02 +0200

    update vite.config and App.jsx so that it can deploy to gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: './',
+   base: './QueueCare/',  //Remove this line if you want to deploy to azure 
 })
