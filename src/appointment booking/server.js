37cab54e933bdbf34e9606c9f40fafdf7ee35cd8
Nthabiseng Lefophana <nthabiseng.lefophana1@gmail.com>
2026-04-21 01:40:42 +0200

    Fix web.config for server subdirectory

--- src/appointment booking/server.js
+++ src/appointment booking/server.js
@@ -1,1 +1,10 @@
-import './servers/server.js';
+import env from 'dotenv';
+env.config();
+
+import app from './servers/app.js'; 
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`${new Date().toLocaleDateString()} Server is running on port ${port}`);
+});
