19ca9991296e541bb4c941851e2b3c8e82c7a066
AlteredFate <128898925+AlteredFate@users.noreply.github.com>
2026-04-20 13:44:21 +0200

    removed emulator setup file

--- src/tests/vitest.setup.js
+++
@@ -1,5 +0,0 @@
-import "@testing-library/jest-dom";
-import { connectAuthEmulator } from "firebase/auth";
-import { auth } from "../firebase";
-
-connectAuthEmulator(auth, "http://127.0.0.1:9099", { disableWarnings: true });
