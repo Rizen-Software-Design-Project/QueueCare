e519b16ed265373ab9ff79cf6ff693f31f41db5d
Nthabiseng Lefophana <nthabiseng.lefophana1@gmail.com>
2026-04-29 19:35:25 +0200

    fixing server.js for azure

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import env from 'dotenv';
+env.config();
 
-const { default: app } = await import('./src/appointment-booking/servers/app.js');
+import app from './src/appointment-booking/servers/app.js';
+ 
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-  console.log(`${new Date().toLocaleDateString()} Server is running on port ${port}`);
+    console.log(`${new Date().toLocaleDateString()} Server is running on port ${port}`);
 });
