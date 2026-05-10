51da6040cd214a219aa0503793d2a95dee3ff257
Hlulani-B <baloyihlulani15@gmail.com>
2026-05-10 11:03:01 +0200

    fixed createschedule

--- src/queueApi.js
+++ src/queueApi.js
@@ -116,6 +116,8 @@ async function checkStaff(staff_id) {
 
 export async function createSchedule(rows) {
   const staffId = rows?.[0]?.staff_id;
+ //Supposed to delete first to prevent duplicates
+ deleteSchedule(staffID);
 
   if (!staffId) {
     return { success: false, error: "Missing staff_id" };
