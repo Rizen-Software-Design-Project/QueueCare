d5655f84d0a3a2896f5b723f9de91dd0251d1c2c
AlteredFate <128898925+AlteredFate@users.noreply.github.com>
2026-05-11 14:07:17 +0200

    fixing import in Walkin.test

--- src/components/Walkin.test.jsx
+++ src/components/Walkin.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import { describe, it, expect, vi, beforeEach } from "vitest";
-import WalkIn from "./WalkIn";
+import WalkIn from "./Walkin";
 import userEvent from "@testing-library/user-event";
 
 
