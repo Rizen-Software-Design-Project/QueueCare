12fb578ce469dea5379f49de7e3fb4edc118c877
Hlulani-B <baloyihlulani15@gmail.com>
2026-05-17 11:26:36 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import QueueCarePolicy from './App.jsx'
+import QueueCarePolicy from './ServicePolicy.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
